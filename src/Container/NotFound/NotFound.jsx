cd3e603dd0dff39e53c9473f27a48d7a608ea7f6
Honey Patel <honey.p@simformsolutions.com>
2023-06-07 16:46:12 +0530

    not found page modified

--- src/Container/NotFound/NotFound.jsx
+++ src/Container/NotFound/NotFound.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import notFoundImage from '../../assets/Images/Not_Found_Error_Image.jpg'
+
 
 const NotFound = () => {
+
+
+
     return (
-        <div>
-            Not Found
-        </div>
+        <>
+            <img src={notFoundImage} alt="Not Found" style={{ maxHeight: "20em", maxWidth: "20em", display: "flex", alignItems: "center", margin: "0 auto", marginTop: "10em" }} />
+        </>
     )
 }
 
