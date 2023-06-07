acec988fa02be2d2548bf73025a6de160c486905
Honey Patel <honey.p@simformsolutions.com>
2023-06-07 16:54:58 +0530

    netlify.toml file added

--- src/Container/NotFound/NotFound.jsx
+++ src/Container/NotFound/NotFound.jsx
@@ -4,8 +4,6 @@ import notFoundImage from '../../assets/Images/Not_Found_Error_Image.jpg'
 
 const NotFound = () => {
 
-
-
     return (
         <>
             <img src={notFoundImage} alt="Not Found" style={{ maxHeight: "20em", maxWidth: "20em", display: "flex", alignItems: "center", margin: "0 auto", marginTop: "10em" }} />
