c4ec8293ced558231ea8be4570bcaad649113572
Honey Patel <honey.p@simformsolutions.com>
2023-06-20 10:06:50 +0530

    not registered user error modified

--- src/Services/user.js
+++ src/Services/user.js
@@ -1,7 +1,6 @@
 
 
 import { spotifyApi } from "./spotify";
-import { authActions } from "../Store/authSlice";
 
 
 export const fetchUserProfile = async () => {
@@ -11,6 +10,7 @@ export const fetchUserProfile = async () => {
             return { name: response?.display_name, email: response?.email, followers: response?.followers, image: response?.images, id: response.id };
         }
     } catch (error) {
-        throw new Error(error.status)
+        const customError = { status: error.status, message: error.response };
+        throw customError;
     }
 };
--- src/Store/authSlice.jsx
+++ src/Store/authSlice.jsx
@@ -1,7 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { fetchUserProfile } from '../Services/user';
 import { spotifyApi } from '../Services/spotify';
-import { useDispatch } from 'react-redux';
 
 export const fetchUserData = createAsyncThunk(
     'user/fetchUserData',
@@ -10,10 +9,8 @@ export const fetchUserData = createAsyncThunk(
             const user = await fetchUserProfile();
             return user;
         } catch (error) {
-            if (error.message == 401) {
-                dispatch(authActions.removeAccessToken());
-            }
-            return rejectWithValue({ message: error.message, status: error.status });
+            dispatch(authActions.removeAccessToken());
+            return rejectWithValue(error);
         }
     }
 );
@@ -46,19 +43,15 @@ const authSlice = createSlice({
                 state.error = null;
             })
             .addCase(fetchUserData.fulfilled, (state, action) => {
+                state.error = null;
                 state.user = action.payload;
                 state.loading = false;
             })
             .addCase(fetchUserData.rejected, (state, action) => {
-                console.log(action);
-                // state.loading = false;
-                // if (action.error.message == 403) {
-                //     state.error = action.error.message;
-                // }
-                // if (action.error.message == 401) {
-                //     console.log("here");
-                //     dispatch(authActions.removeAccessToken())
-                // }
+                state.loading = false;
+                if (action.payload.status === 403) {
+                    state.error = action.payload.message;
+                }
             });
     },
 });
